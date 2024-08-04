70670a278e4f5410051d6df8425d521528c1fbb4
igie <baldesansoigieboysanchez@gmail.com>
2024-08-04 20:37:48 +0800

    textlog

--- src/pages/landingPage/About.tsx
+++ src/pages/landingPage/About.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function About() {
+  console.log("Test");
   return (
     <section className="flex flex-col items-center min-h-[100vh] gap-2  sections"></section>
   );
